4b19388e41281ab7bce7211d41deb0bfc04012c5
nspp99b <chromepanthers@gmail.com>
2018-02-19 15:11:07 -0500

    configured UserInput component and reducer to update state on submit

--- src/components/UserInput.js
+++ src/components/UserInput.js
@@ -1,10 +1,40 @@
 import React, { Component } from 'react';
 
 class UserInput extends Component {
+  
+  state = {
+    username: "",
+    hometown: ""
+  }
+  
+  handleUsernameInput = (e) => {
+    this.setState({
+      username: e.target.value
+    })
+  }
+  
+  handleHometownInput = (e) => {
+    this.setState({
+      hometown: e.target.value
+    })
+  }
+  
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.store.dispatch({
+      type: 'ADD_USER',
+      user: { username: this.state.username, hometown: this.state.hometown }
+    })
+  }
+  
   render() {
     return(
       <div>
-        user input
+        <form onSubmit={(e) => this.handleSubmit(e)}>
+          <input type="text" onChange={(e) => this.handleUsernameInput(e)} value={this.state.username}></input>
+          <input type="text" onChange={(e) => this.handleHometownInput(e)} value={this.state.hometown}></input>
+          <input type="submit"></input>
+        </form>
       </div>
     );
   }
